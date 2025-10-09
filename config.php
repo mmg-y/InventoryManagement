79a7ad0577beba510f307dfcaac6a436e6f4616f
Belen <belejohnjoseph4812@gmail.com>
2025-10-10 00:26:46 +0800

    Added ignored files

--- config.php
+++ config.php
@@ -2,11 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-<<<<<<< HEAD
-$db   = "super_marketV1";
-=======
 $db   = "super_market";
->>>>>>> belen
 
 $conn = new mysqli($host, $user, $pass, $db);
 
